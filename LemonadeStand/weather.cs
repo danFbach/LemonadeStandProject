da32f73434169d7bd3344fa6015ab1e50c98a934
danFbach <quiktekrepair@gmail.com>
2016-03-23 18:00:33 -0500

    some classes stubbed out, a tiny bit of weather class started

---
+++ LemonadeStand/weather.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LemonadeStand
+{
+    class weather
+    {
+        public void weatherSelection()
+        {
+            public Random randomWeather;
+        public int weatherNumber = randomWeather.Next(1, 5);
+    }
+            switch(weatherNumber)
+                case (1)://cool thunderstorms - fewest customers
+                    break;
+                case (2)://cool rainy - fewer customers
+                    break;
+                case (3)://warm partly cloudy - some customers
+                    break;
+                case (4)://warm sunny - more customers
+                    break;
+                case (5)://hot sunny - most customers
+                    break;
+                default:
+                    break;
+            
+        }
+    }
+}
+
+
+
+
+        
+        
+        
+        
+    }
+}
